ff768fa86f3840af995636a39d5c37fa9ac03968
jre08 <jre08@hotmail.com>
2016-02-07 21:08:49 -0600

    SharpEditor

---
+++ SharpEditor/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace SharpEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ImportDirDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnOpenImage = new System.Windows.Forms.ToolStripButton();
+            this.btnImportFolder = new System.Windows.Forms.ToolStripButton();
+            this.saveToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.printToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.helpToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.trckbrEdit = new System.Windows.Forms.TrackBar();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trckbrEdit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(12, 134);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(218, 728);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged_1);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(128, 128);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(236, 134);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(680, 719);
+            this.panel1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(674, 713);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseUp);
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFile";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnOpenImage,
+            this.btnImportFolder,
+            this.saveToolStripButton,
+            this.toolStripButton1,
+            this.printToolStripButton,
+            this.toolStripSeparator,
+            this.helpToolStripButton});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(928, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnOpenImage
+            // 
+            this.btnOpenImage.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnOpenImage.Image = ((System.Drawing.Image)(resources.GetObject("btnOpenImage.Image")));
+            this.btnOpenImage.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnOpenImage.Name = "btnOpenImage";
+            this.btnOpenImage.Size = new System.Drawing.Size(23, 22);
+            this.btnOpenImage.Text = "&New";
+            this.btnOpenImage.Click += new System.EventHandler(this.btnOpenImage_Click);
+            // 
+            // btnImportFolder
+            // 
+            this.btnImportFolder.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnImportFolder.Image = ((System.Drawing.Image)(resources.GetObject("btnImportFolder.Image")));
+            this.btnImportFolder.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnImportFolder.Name = "btnImportFolder";
+            this.btnImportFolder.Size = new System.Drawing.Size(23, 22);
+            this.btnImportFolder.Text = "&Open";
+            this.btnImportFolder.Click += new System.EventHandler(this.btnImportFolder_Click);
+            // 
+            // saveToolStripButton
+            // 
+            this.saveToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.saveToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("saveToolStripButton.Image")));
+            this.saveToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.saveToolStripButton.Name = "saveToolStripButton";
+            this.saveToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.saveToolStripButton.Text = "&Save";
+            this.saveToolStripButton.Click += new System.EventHandler(this.saveToolStripButton_Click);
+            // 
+            // printToolStripButton
+            // 
+            this.printToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.printToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("printToolStripButton.Image")));
+            this.printToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.printToolStripButton.Name = "printToolStripButton";
+            this.printToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.printToolStripButton.Text = "&Print";
+            // 
+            // toolStripSeparator
+            // 
+            this.toolStripSeparator.Name = "toolStripSeparator";
+            this.toolStripSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // helpToolStripButton
+            // 
+            this.helpToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.helpToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("helpToolStripButton.Image")));
+            this.helpToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.helpToolStripButton.Name = "helpToolStripButton";
+            this.helpToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.helpToolStripButton.Text = "He&lp";
+            // 
+            // trckbrEdit
+            // 
+            this.trckbrEdit.LargeChange = 1;
+            this.trckbrEdit.Location = new System.Drawing.Point(236, 28);
+            this.trckbrEdit.Maximum = 1;
+            this.trckbrEdit.Name = "trckbrEdit";
+            this.trckbrEdit.Size = new System.Drawing.Size(58, 45);
+            this.trckbrEdit.TabIndex = 3;
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = global::SharpEditor.Properties.Resources.Button_Close;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(928, 865);
+            this.Controls.Add(this.trckbrEdit);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trckbrEdit)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FolderBrowserDialog ImportDirDialog;
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnOpenImage;
+        private System.Windows.Forms.ToolStripButton btnImportFolder;
+        private System.Windows.Forms.ToolStripButton saveToolStripButton;
+        private System.Windows.Forms.ToolStripButton printToolStripButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator;
+        private System.Windows.Forms.ToolStripButton helpToolStripButton;
+        private System.Windows.Forms.TrackBar trckbrEdit;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+    }
+}
+
---
+++ SharpEditor/Form1.cs
@@ -0,0 +1,408 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Printing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Web;
+using System.Diagnostics;
+
+namespace SharpEditor
+{
+
+    public partial class Form1 : Form
+    {
+        DirectoryInfo tempdi;
+        Point startCorner, up, down;
+        Rectangle rubberBand;
+        Color rubberBandColor = Color.Red;
+        Boolean rubberBanding = false, importedDir = false, importedPDF = false;
+        int listNum, currentPage = 0, totalPages, finishX, finishY, startX, startY;
+        string strFilePath, filename, tempfile = "temp.tif", strPath, strFileName;
+        Bitmap srcBmpfrm, srcBmp, imgBmp;
+        Image pic, resized;
+        System.Drawing.Printing.PrintDocument prntDoc;
+        Graphics g;
+        Pen pn;
+
+
+        FileStream fs;
+        private void btnImportFolder_Click(object sender, EventArgs e)
+        {
+            if ((ImportDirDialog.ShowDialog() == DialogResult.OK))
+            {
+                ImportDir();
+
+            }
+
+        }
+
+
+
+        private void listView1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            if (importedPDF == false)
+            {
+                foreach (ListViewItem item in listView1.SelectedItems)
+                {
+                    listNum = item.Index;
+                    srcBmp = null;
+                    fs.Dispose();
+                    fs = File.Open(Application.StartupPath + "\\temp\\temp" + listNum + ".tif", FileMode.Open, FileAccess.ReadWrite);
+                    srcBmp = (Bitmap)Bitmap.FromStream(fs);
+                    //srcBmpfrm = Bitmap.FromFile(Application.StartupPath & "\temp\temp" & item.Index & ".tif")
+                    //srcBmp.SelectActiveFrame(FrameDimension.Page, item.ImageIndex)
+                    //PictureBox1.Image = ResizeImage(srcBmp, New Size(PictureBox1.Width, PictureBox1.Height))
+                    pictureBox1.Width = srcBmp.Width;
+                    pictureBox1.Height = srcBmp.Height;
+                    pictureBox1.Image = srcBmp;
+
+
+                }
+
+
+
+            }
+            else
+            {
+                //    'PictureBox1.Image = ResizeImage(srcBmp, New Size(PictureBox1.Width, PictureBox1.Height))
+                //filename = System.IO.Path.Combine(folder, item.Text)
+                /*
+                if (Rbn_ck_Editmode.Checked == true)
+                {
+                    PictureBox1.Width = picsizew(srcBmp);
+                    PictureBox1.Height = picsizeh(srcBmp);
+                }
+                */
+
+            }
+        }
+
+        private void saveToolStripButton_Click(object sender, EventArgs e)
+        {
+            savemouse();
+        }
+
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenImage_Click(object sender, EventArgs e)
+        {
+            //openFile.ShowDialog();
+            DialogResult result = openFile.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+
+                addimage2(openFile.FileName);
+                ImgList();
+        }
+
+        private void openFile_Fileok(object sender, EventArgs e)
+
+        {
+            addimage2(openFile.FileName);
+            ImgList();
+        }
+
+        void addimage2(string filename)
+        {
+            tempdi = new DirectoryInfo(@Application.StartupPath + "\\temp\\");
+            fs = File.Open(filename, FileMode.Open, FileAccess.ReadWrite);
+            srcBmp =  (Bitmap)Bitmap.FromStream(fs);
+            totalPages = Convert.ToInt32(srcBmp.GetFrameCount(FrameDimension.Page) - 1);
+            int i;
+            for (i = 0; i <= totalPages; i++)
+            {
+                srcBmp.SelectActiveFrame(FrameDimension.Page, i);
+
+                resized = new Bitmap(srcBmp, srcBmp.Width, srcBmp.Height);
+                if (srcBmp.Width > 1200)
+                {
+                    resized = ResizeImage(resized, new Size(Convert.ToInt32(srcBmp.Width / 2.1), Convert.ToInt32(srcBmp.Height / 2.1)), true);
+                }
+                int num = tempdi.GetFiles().Count();
+                resized.Save(Application.StartupPath + "\\temp\\temp" + num + ".tif", System.Drawing.Imaging.ImageFormat.Tiff);
+                resized.Dispose();
+            }
+            fs.Dispose();
+            //PictureBox1.Image = ResizeImage(srcBmp, New Size(PictureBox1.Width, PictureBox1.Height))
+            //ListView1.Items.Item(0).Selected = True
+        }
+
+        public static Image ResizeImage(Image image, Size size, bool preserveAspectRatio)
+        {
+            int newWidth = 0;
+            int newHeight = 0;
+            if (preserveAspectRatio == true )
+            {
+                int originalWidth = image.Width;
+                int originalHeight = image.Height;
+                float percentWidth = Convert.ToSingle(size.Width) / Convert.ToSingle(originalWidth);
+                float percentHeight = Convert.ToSingle(size.Height) / Convert.ToSingle(originalHeight);
+                float percent = percentHeight < percentWidth ? percentHeight : percentWidth;
+                newWidth = Convert.ToInt32(originalWidth * percent);
+                newHeight = Convert.ToInt32(originalHeight * percent);    
+            }
+            else
+            {
+                newWidth = size.Width;
+                newHeight = size.Height;
+               
+            }
+            Image newImage = new Bitmap(newWidth, newHeight);
+            using (Graphics graphicsHandle = Graphics.FromImage(newImage))
+            {
+                graphicsHandle.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphicsHandle.DrawImage(image, 0, 0, newWidth, newHeight);
+            }
+            return newImage;
+        }
+
+        #region "Paint Rectangle On Mouse move"
+        /*
+        Point startPos;      // mouse-down position
+        Point currentPos;    // current mouse position
+        bool drawing;        // busy drawing
+        List<Rectangle> rectangles = new List<Rectangle>();  // previous rectangles
+
+        private Rectangle getRectangle()
+        {
+            return new Rectangle(
+                Math.Min(startPos.X, currentPos.X),
+                Math.Min(startPos.Y, currentPos.Y),
+                Math.Abs(startPos.X - currentPos.X),
+                Math.Abs(startPos.Y - currentPos.Y));
+        }
+
+        private void canevas_MouseDown(object sender, MouseEventArgs e)
+        {
+            currentPos = startPos = e.Location;
+            drawing = true;
+        }
+
+        private void canevas_MouseMove(object sender, MouseEventArgs e)
+        {
+            currentPos = e.Location;
+            if (drawing) canevas.Invalidate();
+        }
+
+        private void canevas_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (drawing)
+            {
+                drawing = false;
+                var rc = getRectangle();
+                if (rc.Width > 0 && rc.Height > 0) rectangles.Add(rc);
+                canevas.Invalidate();
+            }
+        }
+
+        private void canevas_Paint(object sender, PaintEventArgs e)
+        {
+            if (rectangles.Count > 0) e.Graphics.DrawRectangles(Pens.Black, rectangles.ToArray());
+            if (drawing) e.Graphics.DrawRectangle(Pens.Red, getRectangle());
+        }
+
+        */
+        private void PictureBox1_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left & trckbrEdit.Value == 1)
+            {
+                rubberBanding = true;
+                startCorner = e.Location;
+                rubberBand = Rectangle.Empty;
+                down = new Point(startCorner.X, startCorner.Y);
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void PictureBox1_MouseMove(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (rubberBanding)
+            {
+                rubberBand.Width = Math.Abs(e.X - startCorner.X);
+                rubberBand.Height = Math.Abs(e.Y - startCorner.Y);
+                rubberBand.X = Math.Min(e.X, startCorner.X);
+                rubberBand.Y = Math.Min(e.Y, startCorner.Y);
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void PictureBox1_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+
+            if (e.Button == MouseButtons.Left & trckbrEdit.Value == 1)
+            {
+               System.Drawing.Image img = default(System.Drawing.Image);
+                //sets the current page as image.
+                img = new Bitmap(pictureBox1.Image);
+
+                Bitmap objBmp = new Bitmap(img, img.Width, img.Height);
+                Bitmap objNewBmp = new Bitmap(objBmp.Width, objBmp.Height, System.Drawing.Imaging.PixelFormat.Format16bppRgb555);
+
+                Graphics g = default(Graphics);
+                g = Graphics.FromImage(objNewBmp);
+                //Creats a duplicate image file as bitmap format 
+                Rectangle rect = default(Rectangle);
+                var _with1 = rect;
+                _with1.Width = img.Width;
+                _with1.Height = img.Height;
+                _with1.X = 0;
+                _with1.Y = 0;
+
+                g.DrawImage(img, rect);
+
+                Rectangle DrawRect = default(Rectangle);
+                //Sets the position of the mouse
+                finishX = e.X;
+                finishY = e.Y;
+
+                up = new Point(finishX, finishY);
+                //Sets the value of rectangle, x, y, width, height position
+                DrawRect = new Rectangle(Math.Min(up.X, down.X), Math.Min(up.Y, down.Y), Math.Abs(up.X - down.X), Math.Abs(up.Y - down.Y));
+
+                SolidBrush b = new SolidBrush(Color.White);
+                g.FillRectangle(b, DrawRect);
+
+                //Creates an rectagnle on the picture box for visual.
+                g = pictureBox1.CreateGraphics();
+                g.FillRectangle(b, DrawRect);
+                g.Dispose();
+                //objNewBmp.Save("c:\temp\s" & ".tif", Imaging.ImageFormat.Tiff)
+                pictureBox1.Image = objNewBmp;
+                //cboFrameEdit.Items.Add(curF)
+                g.Dispose();
+                objBmp.Dispose();
+                //objNewBmp.Dispose()
+
+                rubberBanding = false;
+
+                //savemouse()
+
+            }
+            //pn.Dispose()
+        }
+
+        private void PictureBox1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            using (Pen pn = new Pen(rubberBandColor) { DashStyle = System.Drawing.Drawing2D.DashStyle.Dash })
+            {
+                e.Graphics.DrawRectangle(pn, rubberBand);
+            }
+
+        }
+
+
+
+
+        #endregion
+
+
+        public void ImgList()
+        {
+            this.Cursor = Cursors.WaitCursor;
+            listView1.Items.Clear();
+
+            imageList1.Images.Clear();
+            imgBmp = null;
+            tempdi = new DirectoryInfo(Application.StartupPath + "\\temp\\");
+            dynamic num = 0;
+            int i;
+            for (i = 0; i <= tempdi.GetFiles().Count() - 1; i++)
+            {
+                num = i;
+                 fs = File.Open(Application.StartupPath + "\\temp\\temp" + num + ".tif", FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
+                imgBmp = (Bitmap)Bitmap.FromStream(fs);
+                //Image imgBmp = Bitmap.FromFile(Application.StartupPath + "\\temp\\temp" + num + ".tif");
+                imageList1.Images.Add("ico" + num, imgBmp);
+                //imageList1.Images.Add(num, Application.StartupPath + "\\temp\\temp" + num + ".tif");
+                listView1.Items.Add(Convert.ToString(num), "Page" + Convert.ToString(num + 1), num);
+                fs.Dispose();
+                //num += 1;
+            }
+            this.Cursor = Cursors.Default;
+        }
+
+        public void savemouse()
+        {
+            //'Saves the picturebox image in a temp folder
+            Bitmap objNewBmp = new Bitmap(pictureBox1.Image);
+            Graphics g = default(Graphics);
+
+            foreach (ListViewItem item in listView1.SelectedItems)
+            {
+                listNum = item.Index;
+            }
+
+            //ImageList1.Images.Clear()
+            //ListView1.Items.Clear()
+            srcBmp = null;
+            fs.Dispose(); 
+            //PictureBox1.Image.Dispose()
+            //delete the current temp file to be overwritten by the new edited temp file
+           File.Delete(Application.StartupPath + "\\temp\\temp" + listNum + ".tif");
+            g = Graphics.FromImage(objNewBmp);
+            //'Creats a duplicate image file as bitmap format 
+
+            //'Creates an rectagnle on the picture box for visual.
+            g = pictureBox1.CreateGraphics();
+            objNewBmp.Save(Application.StartupPath + "\\temp\\temp" + listNum + ".tif", System.Drawing.Imaging.ImageFormat.Tiff);
+
+            //ListView1.Refresh()
+            ImgList();
+            //ListView1.Items.Item(ListNum).Selected = True
+        }
+
+
+        private string[] allowedExtensions = {
+    ".gif",
+    ".jpg",
+    ".bmp",
+    ".png",
+    ".tiff",
+    ".tif"
+};
+
+        public void ImportDir()
+        {
+            DirectoryInfo di = new DirectoryInfo(ImportDirDialog.SelectedPath);
+            foreach (FileInfo  fi in di.GetFiles())
+            {
+               
+                foreach (string extension in allowedExtensions)
+                {
+                    if (extension == System.IO.Path.GetExtension(fi.FullName))
+                    {
+                        Debug.Print(fi.Name);
+                        addimage2(fi.FullName);
+                        //ImageList1.Images.Add(Image.FromFile(dirFile))
+
+                    }
+                }
+                //If fi.Extension = ".jpg" Then
+
+                //End If
+            }
+
+           // dirPath.Text = ImportDirDialog.SelectedPath;
+            importedDir = true;
+            ImgList();
+        }
+
+
+    }
+
+
+}
